fffdf28a2349d6d4f752c2146c028a555aaed521
Kapcsándi Tamás <tkapcsandi@combit.hu>
2021-01-11 19:55:09 +0100

    findPeak updated

--- src/main/java/main/java/algorithms/DivideAndConquer.java
+++ src/main/java/main/java/algorithms/DivideAndConquer.java
@@ -1,21 +1,36 @@
 package main.java.algorithms;
 
-import com.sun.istack.internal.NotNull;
+import lombok.NonNull;
 
 public class DivideAndConquer {
     /**
      * Use divide & conquer to find a local peak, divide the inputArray n/2 part and search for a peak in a smaller part
      *
      * @param inputArray need to find a peak in this array
+     *
      * @return index of a local peak
      */
-    public static Integer findAPeak(int[] inputArray) throws NullPointerException, IllegalArgumentException{
-        if(inputArray == null) throw new NullPointerException();
-        if(inputArray.length == 0) throw  new IllegalArgumentException();
+    public Integer findAPeak(final @NonNull int[] inputArray) {
+        checkIsArrayEmpty(inputArray);
+        checkIsPeakAvailable(inputArray);
+        return lookForPeak(inputArray);
+    }
 
+    private void checkIsArrayEmpty(int[] inputArray) {
+        if (inputArray.length == 0) {
+            throw new IllegalArgumentException("InputArray can not be empty");
+        }
+    }
+
+    private void checkIsPeakAvailable(int[] inputArray){
+        if(inputArray.length < 3 ){
+            throw new IllegalArgumentException("Can not find peak in array, which is smaller than 3");
+        }
+    }
+
+    private int lookForPeak(int[] inputArray) {
         int lowerIndex = 1;
         int higherIndex = inputArray.length - 2;
-
         while (lowerIndex <= higherIndex) {
             int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
             if (inputArray[middle] < inputArray[middle - 1]) {
@@ -35,11 +50,17 @@ public class DivideAndConquer {
      * @param inputArray  need to find a peak in this array
      * @param lowerIndex  left bound to the search interval
      * @param higherIndex right bound to the search interval
+     *
      * @return index of a local peak
      */
-    public static Integer findAPeakRecursive(int[] inputArray, int lowerIndex, int higherIndex) throws NullPointerException, IllegalArgumentException{
-        if(inputArray == null) throw new NullPointerException();
-        if(inputArray.length == 0 || ( lowerIndex < 0 || lowerIndex > higherIndex) || (higherIndex < lowerIndex || higherIndex > inputArray.length - 1)) throw  new IllegalArgumentException();
+    public static Integer findAPeakRecursive(int[] inputArray, int lowerIndex, int higherIndex)
+        throws NullPointerException, IllegalArgumentException {
+        if (inputArray == null) {
+            throw new NullPointerException();
+        }
+        if (inputArray.length == 0 || (lowerIndex < 0 || lowerIndex > higherIndex) || (higherIndex < lowerIndex || higherIndex > inputArray.length - 1)) {
+            throw new IllegalArgumentException();
+        }
 
         if (higherIndex - lowerIndex <= 1) {
             if (inputArray[lowerIndex] >= inputArray[lowerIndex + 1]) {
@@ -60,12 +81,13 @@ public class DivideAndConquer {
 
     /**
      * Search in an ordered input for specified key
+     *
      * @param inputArray
      * @param leftBound
      * @param rightBound
      * @param key
-     * @return
-     * - the index if the key,
+     *
+     * @return - the index if the key,
      * - -1 the array does not contains the key
      */
     public static int binarySearch(int[] inputArray, int leftBound, int rightBound, int key) {
@@ -83,5 +105,4 @@ public class DivideAndConquer {
             return binarySearch(inputArray, middle + 1, rightBound, key);
         }
     }
-
 }
